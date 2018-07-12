5e1a74974a3292afbc296e857d168113a86cd25d
Unknown <spencerdu@hotmail.co.uk>
2018-07-12 16:04:19 +0100

    BC23 Using struct for Simple Types

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -4,7 +4,15 @@
 using FString = std::string;
 using int32 = int;
 
-class FBullCowGame {
+// all values intialised to zero
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+class FBullCowGame
+{
 public:
 	FBullCowGame(); // constructor
 
@@ -14,11 +22,13 @@ public:
 
 	void Reset(); // TODO make a more rich return value.
 	bool CheckGuessValidity(FString); // TODO make a more rich return value.
-									  // provide a method for counting bulls & cows, and increasing try #
+	BullCowCount SubmitGuess(FString);
 
-// ^^ Please try and ignore this and focus on the interface above ^^
+
+	// ^^ Please try and ignore this and focus on the interface above ^^
 private:
 	// see constructor for initialisation
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
+	FString MyHiddenWord;
 };
